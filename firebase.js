eb1529bed7aa4f7c4a1a9c62ee0c38985227049b
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-04-08 21:27:43 -0300

    API key in .env and fonts for Home

--- firebase.js
+++ firebase.js
@@ -1,15 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, doc, addDoc, getDocs, deleteDoc, updateDoc, getDoc, collectionGroup } from "firebase/firestore";
+import dotenv from 'dotenv'; 
 
+dotenv.config();
 
 var firebaseConfig = {
-  apiKey: "AIzaSyAFl7RBfhV8vgbjOff-elGk7UyAIj55ur4",
+  apiKey: process.env.API_KEY,
   authDomain: "react-blog-silvipalaoro.firebaseapp.com",
   projectId: "react-blog-silvipalaoro",
   storageBucket: "react-blog-silvipalaoro.firebasestorage.app",
-  messagingSenderId: "319447413809",
-  appId: "1:319447413809:web:eda6524defd449cd225110"
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -17,3 +20,13 @@ const db = getFirestore(app);
 
 // export const firestore = app.firestore;
 export { db, collection, doc, addDoc, getDocs, deleteDoc, updateDoc, getDoc, collectionGroup };
+
+
+// var firebaseConfig = {
+//   apiKey: "AIzaSyAFl7RBfhV8vgbjOff-elGk7UyAIj55ur4",
+//   authDomain: "react-blog-silvipalaoro.firebaseapp.com",
+//   projectId: "react-blog-silvipalaoro",
+//   storageBucket: "react-blog-silvipalaoro.firebasestorage.app",
+//   messagingSenderId: "319447413809",
+//   appId: "1:319447413809:web:eda6524defd449cd225110"
+// };
