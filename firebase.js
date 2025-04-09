6bb889b369e751449fefd3baec94b7c63966b3ab
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-04-09 10:57:14 -0300

    cleaned code

--- firebase.js
+++ firebase.js
@@ -20,13 +20,3 @@ const db = getFirestore(app);
 
 // export const firestore = app.firestore;
 export { db, collection, doc, addDoc, getDocs, deleteDoc, updateDoc, getDoc, collectionGroup };
-
-
-// var firebaseConfig = {
-//   apiKey: "AIzaSyAFl7RBfhV8vgbjOff-elGk7UyAIj55ur4",
-//   authDomain: "react-blog-silvipalaoro.firebaseapp.com",
-//   projectId: "react-blog-silvipalaoro",
-//   storageBucket: "react-blog-silvipalaoro.firebasestorage.app",
-//   messagingSenderId: "319447413809",
-//   appId: "1:319447413809:web:eda6524defd449cd225110"
-// };
